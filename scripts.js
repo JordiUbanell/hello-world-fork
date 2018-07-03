1b38c5c6148ba7faa7e36ec86cd5491dced839ba
SergioCastilloSorribes <38652141+SergioCastilloSorribes@users.noreply.github.com>
2018-07-03 12:07:36 +0200

    add alert in welcome

--- scripts.js
+++ scripts.js
@@ -1,3 +1,6 @@
 function showMessage(){
     alert("Hello World! Hola Mundo! Hallo Welt");
 }
+function showInstruction(){
+    alert("La primera instrucción es hacer esto. La segunda instrucción es hacer esto. La tercera instrucción es hacer esto. La cuarta instrucción es hacer esto");
+}
